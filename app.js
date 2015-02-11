334d8505b599a2a485c24b0d530838b9b434553c
James Fazio <james@thefazios.com>
2015-02-11 15:37:23 -0800

    App now has access to categories api

--- app.js
+++ app.js
@@ -6,12 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var videos = require('./routes/videos');
+var categories = require('./routes/categories');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -19,8 +16,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/videos', videos);
+app.use('/categories', categories);
 
 module.exports = app;
